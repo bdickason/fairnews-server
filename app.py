3e350cbfb573a6e0d236538f6e900aeca8c04513
Brad Dickason <dickason@gmail.com>
2019-09-03 17:46:20 -0700

    Migrated config to separate file

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-# Import environment variables (API Keys, etc)
-from dotenv import load_dotenv
-load_dotenv()
-
-
-# Read environment variables
-import os
-api_key = os.environ['NEWS_API'] 
+from lib.config import Config
 
 # Setup News API
 from newsapi import NewsApiClient
 
-newsapi = NewsApiClient(api_key)
+config = Config()
+
+newsapi = NewsApiClient(config.api_key)
 
 top_headlines = newsapi.get_top_headlines(sources='cnn, fox-news')
 
