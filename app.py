9262a50d9c7e9d4bf663fcd5ea71ccb76ed48a5f
Brad Dickason <dickason@gmail.com>
2019-09-04 09:30:27 -0700

    Endpoint now returns json (vs plaintext)

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ config = Config()
 news = News(config.api_key)
 
 # Initialize web server
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Default route - display headlines from common sources
 @app.route('/')
 def headlines():
-    return news.headlines()
+    response = news.headlines()
+    return jsonify(response)
+
